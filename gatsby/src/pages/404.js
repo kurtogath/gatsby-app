4b44659594176965a9bb8d7fb9e60413578a6a5d
Oscar Marti <kurtogath@gmail.com>
2020-10-10 12:00:50 +0200

    Module 1 & 2 finished

---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function For0hForPage() {
+  return (
+    <div>
+      <p>404, que no esiste</p>
+    </div>
+  );
+}
---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+export default function HomePage() {
+    return (
+        <>
+            <p> INDEX PAGE!</p>
+        </>
+    );
+}
